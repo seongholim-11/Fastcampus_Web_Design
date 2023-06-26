56e988689f0b97ecc6aa9249a3c8ae5d569d31bf
seongho <damadu01@gmail.com>
2023-06-26 17:56:30 +0900

    링크연결

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
-const swiper = new Swiper('main', {
+const swiper = new Swiper('.swiper', {
     // Optional parameters
-    direction: 'vertical',
+    direction: 'horizontal',
     loop: true,
   
     // If we need pagination
@@ -12,10 +12,5 @@ const swiper = new Swiper('main', {
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
+    }
   });
