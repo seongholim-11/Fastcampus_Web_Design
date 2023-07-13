c1a5ea7d239c6f5f6e6d5a1c77518f93e74bb780
seongho <damadu01@gmail.com>
2023-07-13 21:50:52 +0900

    완성

--- 14.시퀀스모션/js/mask.js
+++ 14.시퀀스모션/js/mask.js
@@ -1,9 +1,29 @@
 const wrap = document.querySelector("main");
 const btns = document.querySelectorAll("#navi li");
+const panels = document.querySelectorAll("section article");
 
 btns.forEach((btn, index)=>{
     btn.addEventListener('click', e=>{
-        for(let button of btns) button.classList.remove('on')
+        for(let i = 0; i < btns.length; i++){
+            btns[i].classList.remove('on')
+            
+            if(panels[i].classList.contains('on')){
+                panels[i].classList.add('mask')
+            }
+        }
         btns[index].classList.add('on')
+        panels[index].classList.add('lower')
+
+        setTimeout(() => {
+            for(let i = 0; i < panels.length; i++){
+                if(panels[i].classList.contains('on')){
+                    panels[i].classList.remove('on')
+                    panels[i].classList.remove('mask')
+                }
+            } 
+            panels[index].classList.remove('lower')
+            panels[index].classList.add('on')
+        }, 1400)
     })
 })
+
